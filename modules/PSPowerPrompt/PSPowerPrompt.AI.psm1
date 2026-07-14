6adcb7b2312f517796758222e4e4c3defc035d91
Juan Pablo Llinás Ramírez <coordinacion.innovacion@ccsm.org.co>
2026-07-14 07:55:14 -0500

    feat: add multi-provider intelligent assistance

---
+++ modules/PSPowerPrompt/PSPowerPrompt.AI.psm1
@@ -0,0 +1,18 @@
+Set-StrictMode -Version Latest
+
+function Get-PPAIConfigPath {
+    Join-Path $HOME '.ps-powerprompt\config\settings.json'
+}
+
+function Get-PPAIConfig {
+    $path = Get-PPAIConfigPath
+    if (-not (Test-Path -LiteralPath $path)) {
+        throw "No se encontro la configuracion de PS-PowerPrompt en $path"
+    }
+
+    Get-Content -LiteralPath $path -Raw | ConvertFrom-Json
+}
+
+function Save-PPAIConfig {
+    param([Parameter(Mandatory)]$Config)
+    $
